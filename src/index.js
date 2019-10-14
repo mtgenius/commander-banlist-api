dd7f75febd1e68b217e8211986848a32135a3d46
Charles Stover <github@charlesstover.com>
2019-10-14 16:15:44 -0700

    Update Access-Control-Allow-Origin

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ const http = require('http');
 const banlists = require('./banlists');
 const checkForUpdates = require('./check-for-updates');
 
+const DEFAULT_ACCESS_CONTROL_ALLOW_ORIGIN = 'https://mtgeni.us';
+const ACCESS_CONTROL_ALLOW_ORIGINS = [
+  DEFAULT_ACCESS_CONTROL_ALLOW_ORIGIN,
+  'https://mtgenius.github.io',
+];
+
 const TWELVE_HOURS_S = 60 * 60 * 12;
 const TWELVE_HOURS_MS = TWELVE_HOURS_S * 1000;
 
@@ -10,10 +16,13 @@ setInterval(checkForUpdates, TWELVE_HOURS_MS);
 checkForUpdates();
 
 // Start the server.
-http.createServer((_, response) => {
+http.createServer((req, response) => {
   response.writeHead(200, {
     'Access-Control-Allow-Methods': 'GET',
-    'Access-Control-Allow-Origin': process.env.ACCESS_CONTROL_ALLOW_ORIGIN,
+    'Access-Control-Allow-Origin':
+      ACCESS_CONTROL_ALLOW_ORIGINS.indexOf(req.headers.origin) !== -1
+        ? req.headers.origin
+        : DEFAULT_ACCESS_CONTROL_ALLOW_ORIGIN,
     'Cache-Control': 'max-age=' + TWELVE_HOURS_S + ', public',
     'Content-Type': 'application/json; charset=utf-8',
     'Expires': new Date(Date.now() + TWELVE_HOURS_MS).toUTCString()
